ebff4d66e6ba9421b5b35cb905d5f73692e11ac9
Nahuel Lattessi <n_lattessi@hotmail.com>
2016-06-02 23:38:21 -0300

    Added Test

---
+++ tests/features/DeleteLinkTest.php
@@ -0,0 +1,42 @@
+<?php
+
+use Laravel\Lumen\Testing\DatabaseMigrations;
+
+class DeleteLinkTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    public function testDeleteLink()
+    {
+        $link = factory(\App\Link::class)->create();
+
+        $this
+            ->seeInDatabase('links', [
+                'id' => $link->id,
+                'title' => $link->title,
+                'url' => $link->url,
+                'description' => $link->description,
+                'created_at' => $link->created_at,
+                'updated_at' => $link->updated_at,
+            ]);
+
+        $this
+            ->delete("/links/{$link->id}")
+            ->seeStatusCode(200)
+            ->notSeeInDatabase('links', [
+                'id' => $link->id,
+                'title' => $link->title,
+                'url' => $link->url,
+                'description' => $link->description,
+                'created_at' => $link->created_at,
+                'updated_at' => $link->updated_at,
+            ]);
+    }
+
+    public function testGet404IfNotExistsLink()
+    {
+        $this
+            ->delete('links/999')
+            ->seeStatusCode(404);
+    }
+}
